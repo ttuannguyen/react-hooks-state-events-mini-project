fd4505b888f18db931bbf6ea67596896501e3808
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-03-13 21:59:11 -0400

    commit tree b26277aef348694627fe3c5f242da16e85195705

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,26 @@ import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+import { useState } from "react/cjs/react.production.min";
+//console.log("Here's the data you're working with");
+//console.log({ CATEGORIES, TASKS });
 
 function App() {
+
+  //const [tasks, setTasks] = useState(TASKS);
+
+  const visibleTasks = TASKS; 
+
+  function handleDeleteTask(deletedTaskText) {
+    //setTasks(tasks.filter((task) => task.text !== deletedTaskText));
+  }
+  
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter />
       <NewTaskForm />
-      <TaskList />
+      <TaskList tasks={visibleTasks} onDeleteTask={handleDeleteTask} />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { TASKS } from "../data";
+import Task from "./Task";
 
-function TaskList() {
+
+function TaskList({tasks}) {
+
+  const taskList = tasks.map((task) => (
+    <Task
+      key = {task.text}
+      text = {task.text} 
+      category = {task.category}
+    />
+  ))
+
+  // const taskList = tasks.map((task) => (
+  //   <Task
+  //     key={task.text}
+  //     text={task.text}
+  //     category={task.category}
+  //   />
+  // ))
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {taskList}
     </div>
   );
 }
